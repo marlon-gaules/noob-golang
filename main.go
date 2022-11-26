f3459d4ace5b3e07adc7f5ef4c1b4eee27ed8666
Marlon Sousa <marloncostaesousa@gmail.com>
2022-11-26 10:17:34 -0300

    adicionando aula 3 - variáveis, Valores, Tipo, e operador curto de declaração

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+var b = "olá e bom dia!"
+
 func main() {
-	fmt.Println("Hello World!")
+	//
+	numerodebytes, erros := fmt.Println("Hello World!", "Oi galera", 500)
+	fmt.Println(numerodebytes, erros)
+
+	//
+	x := 10
+	z := 12.4
+	y := "Olá bom dia!"
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("z: %v, %T\n", z, z)
+	fmt.Printf("y: %v, %T", y, y)
+
+	x = 20
+	x, a := 20, 34
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("a: %v, %T\n", a, a)
+
+	fmt.Println(b)
+
+	c := 44
+	d := 65
+	fmt.Println(c + d)
+	fmt.Println(c == d)
+	fmt.Println(c < d)
 }
