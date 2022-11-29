3aed2fcb22e9ebba088fcfc77472af9780b1a3c6
Marlon Sousa <marloncostaesousa@gmail.com>
2022-11-29 08:37:39 -0300

    Adicionando exercício nível 1 - 1

---
+++ exercicios/exercicios.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//	Nível 1 - 2
+
+func main() {
+	//	Cap. 3 – Exercícios
+	//	Nível 1 - 1
+	//	Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+	//		1. 42
+	//    	2. "James Bond"
+	//    	3. true
+	//	Agora demonstre os valores nestas variáveis, com:
+	//    	1. Uma única declaração print
+	//    	2. Múltiplas declarações print
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	//	Nível 1 - 2
+	//	Use var para declarar três variáveis.
+	//	Elas devem ter package-level scope.
+	//	Não atribua valores a estas variáveis.
+	//	Utilize os seguintes identificadores e tipos para estas variáveis:
+	//		1. Identificador "x" deverá ter tipo int
+	//		2. Identificador "y" deverá ter tipo string
+	//		3. Identificador "z" deverá ter tipo bool
+	//	Na função main:
+	//		1. Demonstre os valores de cada identificador
+	//		2. O compilador atribuiu valores para essas variáveis. Como esses valores se chamam?
+
+}
